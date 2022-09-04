3e7837ead39a7811e3ae9c83bd2b606eaeb30b1e
souhail bouricha <souhailbourigha@gmail.com>
2022-09-04 20:31:33 +0100

    Create The Table

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
- 
+const updateText = () =>{
+    const range = document.querySelector("#value");
+    const sizeValue = document.querySelector(".sizeValue");
+    range.addEventListener("input",(e) =>{
+        sizeValue.innerText = `${e.target.value} X ${e.target.value}`;
+    });
+}
+const updateTabel = () =>{
+    const range = document.querySelector("#value");
+    range.addEventListener("change",(e) =>{
+        createTable(e.target.value);
+    });
+}
+const createTable = (Number) =>{
+    const game = document.querySelector(".game");
+    const childes = document.querySelectorAll(".childe");
+    gsap.to(game,0,{gridTemplateColumns : `repeat(${Number},1fr)`});
+    gsap.to(game,0,{gridTemplateRows : `repeat(${Number},1fr)`});
+    if(childes){
+        childes.forEach((e) =>{
+            e.remove();
+        });
+    }
+    for (let i = 0; i < (Number * Number); i++) {
+        const childe = document.createElement("div");
+        childe.classList.add(`childe`, `${i + 1}`);
+        game.appendChild(childe);
+        
+    }
+}
+updateText();
+updateTabel();
+
+console.log("gfg");   
